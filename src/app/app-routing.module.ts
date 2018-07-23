55e398f21f6b98e40303bfc205fa41fbd48e93c1
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-23 17:49:59 +0200

    Added RouterOutlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent}
-]
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
