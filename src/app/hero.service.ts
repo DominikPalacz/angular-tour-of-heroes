ba820ed0f33947d7c22fd302139a46057c4f595e
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-24 15:09:05 +0200

    Added: simulate a data server, get heroes with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-heroes'; //xxx
 import { MessageService } from './message.service'
 
 import { Observable, of } from 'rxjs';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  getHeroes(): Observable<Hero[]> {
-    //todo: send the message after fetching the heroes
-    this.messageService.add('From HeroService: fetched heroes');
-    return of(HEROES);
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
-  // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+
+  // getHeroes(): Observable<Hero[]> {
+  //   //todo: send the message after fetching the heroes
+  //   this.messageService.add('From HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  // of(HEROES) returns an Observable<Hero[]> that emits a single value,
+  // the array of mock heroes.
 
   getHero(id: number): Observable<Hero> {
     // todo msg
