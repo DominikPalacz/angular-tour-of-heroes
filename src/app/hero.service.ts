54e557c6b97c6da3118eb2090cb55b51caf32fea
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-24 18:18:51 +0200

    ready to learn more about Angular

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 import { MessageService } from './message.service'
 
 import { Observable, of } from 'rxjs';
@@ -104,22 +104,4 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  // _________________________________________________________________________
-
-  // getHeroes(): Observable<Hero[]> {
-  //   //todo: send the message after fetching the heroes
-  //   this.messageService.add('From HeroService: fetched heroes');
-  //   return of(HEROES);
-  // }
-
-
-
-  // of(HEROES) returns an Observable<Hero[]> that emits a single value,
-  // the array of mock heroes.
-
-  // getHero(id: number): Observable<Hero> {
-  //   // todo msg
-  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
-  //   return of(HEROES.find(hero => hero.id === id))
-  // }
 }
